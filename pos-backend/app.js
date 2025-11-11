192849a77adc85d11c0cc00b2e6fa26bbe790d4d
Vincent Munywoki <vincentmunywoki12@gmail.com>
2025-11-11 13:38:42 +0300

    Final fix: Added pos-backend files directly to repository

---
+++ pos-backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('POS Backend Running');
+});
+
+module.exports = app;

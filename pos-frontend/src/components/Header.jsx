4d22c095f65cfa8afc960b3547534c08200922a2
Vincent Munywoki <vincentmunywoki12@gmail.com>
2025-11-12 10:37:08 +0300

    Created folder structure of pos-frontend

---
+++ pos-frontend/src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => (
+  <header className="bg-blue-600 text-white p-4 flex justify-between items-center">
+    <h1 className="text-xl font-bold">POS Kenya</h1>
+    <div>User: Admin</div>
+  </header>
+);
+
+export default Header;
